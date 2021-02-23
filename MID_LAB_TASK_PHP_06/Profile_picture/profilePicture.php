066685f1c1e9545cb374ba03689878350082578c
zaman <18-38024-2@student.aiub.edu>
2021-02-24 01:41:57 +0800

    lab_task_6

--- MID_LAB_TASK_PHP_06/Profile_picture/profilePicture.php
+++ MID_LAB_TASK_PHP_06/Profile_picture/profilePicture.php
@@ -13,47 +13,6 @@
             </form>
         
         </fieldset>
-        <?php
-            if(isset($_POST['submit']))
-            {
-                $file = $_FILES['file'];
-                
-                $fileName = $_FILES['file']['name'];
-                $fileTmpName = $_FILES['file']['tmp_name'];
-                $fileSize = $_FILES['file']['size'];
-                $fileError = $_FILES['file']['error'];
-                $fileType = $_FILES['file']['type'];
-
-                $fileEXT = explode('.', $fileName);
-                $fileActualExt = strtolower(end($fileEXT));
-
-                $allowed = array('jpeg','jpeg','png');
-
-                if(in_array($fileActualExt, $allowed))
-                {
-                    if($fileError === 0)
-                    {
-                        if($fileSize < 4000)
-                        {
-                            $fileNameNew = uniqid('', true).".".$fileActualExt;
-                            $fileDestination = 'uploads/';
-                            move_uploaded_file($fileDestination, $fileNameNew);
-                        }
-                        else
-                        {
-                            echo "Your file is too big! Not more than 4MB";
-                        }
-                    }
-                    else
-                    {
-                        echo "Erro on uploading your file!";    
-                    }
-                }
-                else
-                {
-                    echo "You cannot upload this type of file!";
-                }
-            }
-        ?>
+        
     </body>
 </html>
